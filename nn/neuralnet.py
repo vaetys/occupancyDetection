9b28ef0944ce896fea6392a1827364ca19ed0cff
Arttu Heikkilä <arttu.i.heikkila@student.jyu.fi>
2018-11-17 11:59:30 +0900

    Initialization of workspace

---
+++ nn/neuralnet.py
@@ -0,0 +1,13 @@
+import os
+import random
+import numpy as np
+import tensorflow as tf
+
+
+#----------------------------------------------------------------
+ROOT_PATH = "./"
+train_data_directory = os.path.join(ROOT_PATH, "data/Training")
+test_data_directory = os.path.join(ROOT_PATH, "data/Testing")
+#----------------------------------------------------------------
+
+print('hello world')
