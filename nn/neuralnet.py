ab6fcfadcb2a3b397f75c9f9b1f84e974a735d52
Arttu Heikkilä <arttu.i.heikkila@student.jyu.fi>
2018-11-17 12:58:10 +0900

    Fix on the dependencies installation

--- nn/neuralnet.py
+++ nn/neuralnet.py
@@ -6,8 +6,10 @@ import tensorflow as tf
 
 #----------------------------------------------------------------
 ROOT_PATH = "./"
-train_data_directory = os.path.join(ROOT_PATH, "data/Training")
-test_data_directory = os.path.join(ROOT_PATH, "data/Testing")
+train_data_directory = os.path.join(ROOT_PATH, "data/occupancy_data/datatraining.txt")
+test_data_directory = os.path.join(ROOT_PATH, "data/occupancy_data/datatest.txt")
 #----------------------------------------------------------------
+
+
 
 print('hello world')
