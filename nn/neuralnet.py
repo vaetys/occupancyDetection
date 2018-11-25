537d83c457ff44c4b6ba0b302c06f50409678324
Arttu Heikkilä <arttu.i.heikkila@student.jyu.fi>
2018-11-25 15:05:27 +0900

    Implementation of the basic network

--- nn/neuralnet.py
+++ nn/neuralnet.py
@@ -3,21 +3,49 @@ import random
 import numpy as np
 import tensorflow as tf
 import filecmp
+from keras.models import Model
+from keras.layers import Input, Dense
 
+seed = 7
+np.random.seed(seed)
 
 #----------------------------------------------------------------
 trainDataPath = '../data/occupancy_data/datatraining.txt'
-# testDataPath = os.path.join(ROOT_PATH, "data/occupancy_data/datatest.txt")
+testDataPath = '../data/occupancy_data/datatest.txt'
 #----------------------------------------------------------------
 
 def loadData(path):
-    data = np.genfromtxt(path, dtype=None, delimiter=',', names=True, skip_header=2, encoding='ASCII')
+    data = np.genfromtxt(path, dtype=float, delimiter=',', skip_header=1, encoding=None, usecols = (2,3,4,5,6))
     return data
 
-def printData():
-    trainData = loadData(trainDataPath)
-    print(trainData[1])
+#Returns a spearate vector out of the last column of the given 2 dimensional numpy array.
+def getLabels(path):
+    return np.genfromtxt(path, dtype=int, delimiter=',', skip_header=1, encoding=None, usecols = (7))
+    
 
-printData()
+#------------------------------KERAS PART
+
+traindata = loadData('../data/occupancy_data/datatraining.txt')
+testdata = loadData('../data/occupancy_data/datatest.txt')
+trainlabels = getLabels(trainDataPath)
+testlabels = getLabels(testDataPath)
+
+
+
+
+inputs = Input(shape=(5,))
+x = Dense(10, activation='relu')(inputs)
+outputs = Dense(1, activation='sigmoid')(x)
+
+model = Model(inputs=inputs, outputs=outputs)
+
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(traindata, trainlabels, validation_data=(testdata, testlabels), epochs=25)
+
+
 
 
